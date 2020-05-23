a58f0c3231f29e9ba0f4130c646c43ca87432d70
Vanderci <vandercijr@gmail.com>
2020-05-23 14:00:12 -0300

    Updates

---
+++ precache-manifest.8db68a85b833e11cc2e246f019bf27ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7227671c1e3eaff0d7ae8d01545d0b2b",
+    "url": "/oplabcfrontend/index.html"
+  },
+  {
+    "revision": "589453b335e6e1c301da",
+    "url": "/oplabcfrontend/static/js/2.0220c7cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/oplabcfrontend/static/js/2.0220c7cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efae16e10f5c41f9e7c2",
+    "url": "/oplabcfrontend/static/js/main.f36ec77d.chunk.js"
+  },
+  {
+    "revision": "537669a288c2c0bd6e1e",
+    "url": "/oplabcfrontend/static/js/runtime-main.b9de9eab.js"
+  }
+]);
